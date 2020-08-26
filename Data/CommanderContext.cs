62d8eb0fbeba23a08fe43608cabc426ad3cde2e8
Nicolly21 <nicollyoliveira21@hotmail.com>
2020-08-26 18:14:25 -0300

    Configurando ConnectionString e DBContext

---
+++ Data/CommanderContext.cs
@@ -0,0 +1,16 @@
+using Commander.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Commander.Data
+{
+    public class CommanderContext : DbContext
+    {
+        public CommanderContext(DbContextOptions<CommanderContext> opt) : base(opt)
+        {
+            
+        }
+
+        //Criando representação do model Command(table) no BD - - No plural
+        public DbSet<Command> Commands { get; set; }
+    }
+}
