26749bff6cdfba85af97f3d995aa01e589e1479b
Nicolly21 <nicollyoliveira21@hotmail.com>
2020-08-29 12:23:39 -0300

    Realizando Migration dos dados internos

---
+++ Migrations/20200828012404_InitialMigration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Commander.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Commands",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),  //Identity -> incrementa 1 em 1
+                    HowTo = table.Column<string>(maxLength: 100, nullable: false),
+                    Line = table.Column<string>(nullable: false),   
+                    Platform = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Commands", x => x.Id); //[Key] in Command.cs
+                });
+        }
+
+        //Down - 
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Commands");
+        }
+    }
+}
--- Models/Command.cs
+++ Models/Command.cs
@@ -1,10 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Commander.Models
 {
     public class Command
     {
+        [Key] //Opcional pois Migrations considera a primeira prop como primary key e not null (padrão)
         public int Id { get; set; }
+
+        [Required] //Para not null
+        [MaxLength(100)] //Tamanho do varchar
         public string HowTo { get; set; }
+
+        [Required]
         public string Line { get; set; }
+
+        [Required]
         public string Platform { get; set; }
     }
 }
