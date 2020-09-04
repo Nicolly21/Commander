cd2bcf3ef6ab43202e4ecb244fd37784e178256d
Nicolly21 <nicollyoliveira21@hotmail.com>
2020-09-04 00:08:58 -0300

    Configurando NewtonJson

--- Startup.cs
+++ Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
 
 namespace Commander
 {
@@ -29,7 +30,9 @@ namespace Commander
         {
             services.AddDbContext<CommanderContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("CommanderConnection")));
 
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(s => {
+                s.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            });
 
             //services.AddScoped<ICommanderRepo, MockCommanderRepo>(); - Swap for SqlCommanderRepo
             services.AddScoped<ICommanderRepo, SqlCommanderRepo>();
